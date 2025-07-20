a592cfdccdebc33ddb18d03233defef2e275768c
Mohit Gupta <mohit16012004@gmail.com>
2025-07-20 15:05:50 +0530

    Add files via upload
    Signed-off-by: Mohit Gupta <mohit16012004@gmail.com>

--- sorting.java
+++ sorting.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
 public class sorting {
 
     public static void bubbleSort(int arr[]){
@@ -45,10 +48,37 @@ public class sorting {
             arr[prev+1] = curr;
         }     
     }
+
+    public static void countSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i = 0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+        int j = 0; 
+        for(int i=0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String args[]){
-        int arr[] = {5, 8, 3, 2, 9};
+        int arr[] = {1, 4, 1, 3, 2, 4, 3, 7, 4};
         // SelectionSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        // Arrays.sort(arr);
+        // Arrays.sort(arr, 0, 3,Collections.reverseOrder());
+        // Arrays.sort(arr);
+        countSort(arr);
         printArr(arr);
+        
+
     }
 }
